600c8827a02c1deb34f958465cdfceaba4125f83
Muhammad Abrar <121477632+abrar35403@users.noreply.github.com>
2025-04-08 11:03:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,31 +6,20 @@ require("dotenv").config();
 
 const app = express();
 
-// Import routes
-const authRoutes = require("./routes/authRoutes");
-const studentRoutes = require("./routes/studentRoutes");
-const announcementRoutes = require("./routes/announcementRoutes");
-const feeRoutes = require("./routes/feeRoutes");
-const facultyStudentRoutes = require("./routes/facultyStudentRoutes");
-const careerRouts = require("./routes/careerRoutes");
-const documentRoutes = require("./routes/documents");
-const settingsRoutes = require("./routes/settingsRoutes");
-
 // Middleware
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(express.static("uploads"));
 
-// Use routes
-app.use("/auth", authRoutes);
-app.use("/students", studentRoutes);
-app.use("/announcements", announcementRoutes);
-app.use("/fees", feeRoutes);
-app.use("/faculty-students", facultyStudentRoutes);
-app.use("/careers", careerRouts);
-app.use("/documents", documentRoutes);
-app.use("/settings", settingsRoutes);
+
+
+// Test API
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "API is working!",
+  });
+});
 
 // Start server
 const PORT = process.env.PORT || 5000;
